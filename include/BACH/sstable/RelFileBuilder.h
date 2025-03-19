f25cfcca98f892c13dddd827d3b695aad708dbf1
partychicken <2074500199@qq.com>
2025-03-19 06:39:54 +0000

    add RelFileBuilder interface

--- include/BACH/sstable/RelFileBuilder.h
+++ include/BACH/sstable/RelFileBuilder.h
@@ -2,11 +2,12 @@
 
 #include <memory>
 #include <vector>
-#include <pair>
+#include <utility>
 #include <sul/dynamic_bitset.hpp>
 #include "BloomFilter.h"
 #include "BACH/file/FileWriter.h"
 #include "BACH/memory/VertexEntry.h"
+#include "BlockBuilder.h"
 #include "BACH/utils/types.h"
 #include "BACH/utils/utils.h"
 
@@ -27,13 +28,18 @@ namespace BACH
         ~RelFileBuilder() = default;
         //void AddFilter(idx_t keys_num, double false_positive);
 
-        void ArrangeCurrentSrcInfo();
         sul::dynamic_bitset<>*  ArrangeRelFileInfo();
-        void AddEdge(vertex_t src, vertex_t dst, edge_property_t edge_property);
+        void SetBlock(BlockBuilder<Key_t>*block_builder);
+        void SetDict();//optional
+        void SetBloomFilter();
     private:
         std::shared_ptr <FileWriter> writer;
         std::shared_ptr<Options> options;
         std::vector<BlockMetaT<Key_t> > block_meta;
         size_t file_size;
+        Key_t key_min, key_max;
+        idx_t block_count;
+        std::shared_ptr<BloomFilter>global_filter;
+
     };
 }

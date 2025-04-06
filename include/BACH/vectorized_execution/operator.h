625491a78a7122c04079e45f50fef56735449c00
bigshark <3366416284@qq.com>
2025-04-06 15:36:34 +0800

    单个过滤条件

--- include/BACH/vectorized_execution/operator.h
+++ include/BACH/vectorized_execution/operator.h
@@ -1,16 +1,17 @@
 #include <bitset>
 #include "../compress/ordered_dictionary.h"
+#include "../utils/types.h"
 
 namespace BACH {
 
-    template <typename Func>
-    void onesiderange(ordereddictionary* dict, idx_t* data, int count, Func* check, bitset<BIT_SIZE>& result){
+    template <typename Func, size_t BIT_SIZE>
+    void onesiderange(OrderedDictionary* dict, idx_t* data, int count, Func* check, std::bitset<BIT_SIZE>& result){
         
-        bool f = check(dict->indexToString[0]);
+        bool f = check(dict->getString(0));
         int l=0, r=count-1;
         while(l<=r) {
             int mid=(l+r)>>1;
-            if (check(dict->indexToString[mid]) == f) l = mid+1;
+            if (check(dict->getString(mid)) == f) l = mid+1;
             else r = mid-1;
         }
         

e4ade70d0d76ce323204e7d53ac264008e153fe3
partychicken <2074500199@qq.com>
2025-05-22 06:32:04 +0000

    reconstruct BlockParser.h, strange bugs not solved

--- include/BACH/sstable/BlockParser.h
+++ include/BACH/sstable/BlockParser.h
@@ -24,32 +24,26 @@ namespace BACH {
                     std::shared_ptr<Options> _options, size_t _offset_in_file,
                     size_t _block_size, idx_t _col_num): reader(_fileReader), options(_options),
                                          offset_in_file(_offset_in_file), block_size(_block_size), col_num(_col_num) {
-            size_t block_end = offset_in_file + block_size;
+            size_t block_end = block_size;
             //size_t offset = 2 * sizeof(idx_t) + sizeof(size_t) + col_num * sizeof(size_t);
             size_t offset = 2 * sizeof(idx_t) + sizeof(size_t);
-            char infobuf[offset];
-            if (!reader->fread(infobuf, offset, block_end - offset)) {
+            data_buffer = static_cast<char*>(malloc(block_size));
+            if (!reader->fread(data_buffer, block_size, offset_in_file)) {
                 std::cout << "read fail begin" << std::endl;
                 ++*(int *) NULL;
             }
+            char *infobuf = data_buffer + block_end - offset;
             util::DecodeFixed(infobuf, key_num);
             util::DecodeFixed(infobuf + sizeof(idx_t), col_num);
             util::DecodeFixed(infobuf + sizeof(idx_t) * 2, key_size);
             size_t col_size_count = col_num * sizeof(size_t);
             size_t col_size_beginpos = block_end - (offset + col_size_count);
-            char colbuf[col_size_count];
-            if (!reader->fread(colbuf, col_size_count, col_size_beginpos)) {
-                std::cout << "read fail begin colsiz" << std::endl;
-                ++*(int *) NULL;
-            }
-
+            char *colbuf = data_buffer + col_size_beginpos;
             col_size = new size_t[col_num];
-
             for (idx_t i = 0; i < col_num; i++) {
                 util::DecodeFixed(colbuf, col_size[i]);
             }
-
-            key_data_endpos = key_size * key_num + offset_in_file;
+            key_data_endpos = key_size * key_num;
             size_t nowpos = key_data_endpos;
             col_data_endpos = static_cast<size_t *>(malloc(col_num * sizeof(size_t)));
             for (idx_t i = 0; i < col_num; i++) {
@@ -66,98 +60,47 @@ namespace BACH {
         }
 
         ~BlockParser() {
+            free(data_buffer);
             free(col_data_endpos);
-            if (col_size)
-                free(col_size);
+            delete[] col_size;
         }
 
         Tuple GetTuple(Key_t key) {
-            idx_t single_read_num = this->options->READ_BUFFER_SIZE / key_size;
-            size_t single_read_size = single_read_num * key_size;
-            int read_times = (key_num - 1) / single_read_num + 1;
-            size_t offset = offset_in_file;
-            for (int i = 0; i < read_times - 1; i++) {
-                char buffer[single_read_size];
-                if (!reader->fread(buffer, single_read_size, offset)) {
-                    std::cout << "read fail dst" << std::endl;
-                    ++*(int *) NULL;
-                }
-                offset += single_read_size;
-                size_t inner_off = 0;
-                for (idx_t j = 0; j < single_read_num; j++) {
-                    Key_t nowkey = util::GetDecodeFixed<Key_t>(buffer + inner_off);
-                    if (nowkey == key) {
-                        return GetTupleWithIdx(key, i * single_read_num + j);
-                    }
-                    inner_off += key_size;
-                }
-            }
-            //specific for the last round
-            char buffer[single_read_size];
-            idx_t tmp_read_num = (key_data_endpos - offset) / key_size;
-            if (!reader->fread(buffer, key_data_endpos - offset, offset)) {
-                std::cout << "read fail dst" << std::endl;
-                ++*(int *) NULL;
-            }
+            char* buffer = data_buffer;
+            idx_t tmp_read_num = key_num;
             size_t inner_off = 0;
             for (idx_t j = 0; j < tmp_read_num; j++) {
                 Key_t nowkey = util::GetDecodeFixed<Key_t>(buffer + inner_off);
                 if (nowkey == key) {
-                    return GetTupleWithIdx(key, (read_times - 1) * single_read_num + j);
+                    return GetTupleWithIdx(key, j);
                 }
                 inner_off += key_size;
             }
             return Tuple();
         }
 
-        //output raw pointers, needing free outside after copy
         void GetKeyCol(Key_t* res) {
-            char *buffer = static_cast<char *>(malloc(key_num * key_size));
-            if (!reader->fread(buffer, key_num * key_size, offset_in_file)) {
-                std::cout << "read fail dst" << std::endl;
-                ++*(int *) NULL;
-            }
-            //if constexpr (std::is_same_v<Key_t, std::string>) {
+            char *buffer = data_buffer;
             for (idx_t i = 0; i < key_num; i++) {
                 res[i] = std::string(buffer + key_size * i, key_size);
             }
-            free(buffer);
-
-            // }
-            // return reinterpret_cast<Key_t *>(buffer);
         }
 
         //output raw pointers, needing free outside after copy
         idx_t *GetValCol(int col_id) {
             if (col_id) {
-                char *buffer = static_cast<char *>(malloc(key_num * col_size[col_id]));
-                if (!reader->fread(buffer, key_num * col_size[col_id], col_data_endpos[col_id - 1])) {
-                    std::cout << "read fail dst" << std::endl;
-                    ++*(int *) NULL;
-                }
+                char *buffer = data_buffer + col_data_endpos[col_id - 1];
                 return reinterpret_cast<idx_t *>(buffer);
             } else {
-                char *buffer = static_cast<char *>(malloc(key_num * col_size[col_id]));
-                if (!reader->fread(buffer, key_num * col_size[col_id], key_data_endpos)) {
-                    std::cout << "read fail dst" << std::endl;
-                    ++*(int *) NULL;
-                }
+                char *buffer = data_buffer + key_data_endpos;
                 return reinterpret_cast<idx_t *>(buffer);
             }
         }
 
         void GetKTuple(idx_t &k, Key_t key, std::vector<Tuple> &res) {
-            idx_t single_read_num = this->options->READ_BUFFER_SIZE / key_size;
-            size_t single_read_size = single_read_num * key_size;
-            size_t offset = offset_in_file;
-            char buffer[single_read_size];
-            idx_t tmp_read_num = (key_data_endpos - offset) / key_size;
-            if (!reader->fread(buffer, key_data_endpos - offset, offset)) {
-                std::cout << "read fail dst" << std::endl;
-                ++*(int *) NULL;
-            }
+            char* buffer = data_buffer;
             size_t inner_off = 0;
-            for (idx_t j = 0; j < tmp_read_num; j++) {
+            for (idx_t j = 0; j < key_num; j++) {
                 Key_t nowkey = util::GetDecodeFixed<Key_t>(buffer + inner_off);
                 if (nowkey >= key) {
                     res.emplace_back(GetTupleWithIdx(key, j));
@@ -199,13 +142,7 @@ namespace BACH {
             for (idx_t i = 0; i < col_num; i++) {
                 size_t now_size = col_size[i];
                 size_t now_begin = i ? col_data_endpos[i - 1] : key_data_endpos;
-                //size_t now_begin = col_data_endpos[i];
-                char buffer[now_size + 1];
-                if (!reader->fread(buffer, now_size, now_begin + idx * now_size)) {
-                    std::cout << "read fail dst" << std::endl;
-                    ++*(int *) NULL;
-                }
-                result.row.push_back(std::string(buffer, now_size));
+                result.row.push_back(std::string(data_buffer + now_begin + idx * now_size, now_size));
             }
             return result;
         }
@@ -218,6 +155,7 @@ namespace BACH {
         size_t *col_size = nullptr;
         size_t key_data_endpos = 0;
         size_t *col_data_endpos = nullptr;
+        char *data_buffer = nullptr;
         bool valid = true;
 
         friend class RelFileParser<Key_t>;
